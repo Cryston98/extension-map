069fe5bfa2962eb37157ec8af663520406d99142
Tamas Roland <rolandtamas98@gmail.com>
2021-03-21 18:46:45 +0200

    Added properties file and property file parsing in program

--- src/mainClient.java
+++ src/mainClient.java
@@ -1,5 +1,6 @@
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
@@ -7,101 +8,102 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
-
+import java.util.Properties;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
+import java.net.URI;
 public class mainClient {
-	   static int  count = 0;
+	   static int count = 0;
 	   static int progress=0;	
-	   static int  countComp=0;
+	   static int countComp=0;
 	
 	public static void main(String[] args) throws JSONException {
-		// TODO Auto-generated method stub
 		
-		if(args.length!=2) {
-			System.out.print("usage: <program> --of <outputfile.json> --ftg <baseRooth>");
-			return;
-		}else 
-		
+		ArrayList<String> arguments = readPropertiesFile();
+		if(arguments == null)
+		{return;}
+		else
 		{
-		String outputFile=args[0];
-		String path=args[1];
-		
-		getFile(path);
-		System.out.println("size: "+count);
-		ArrayList<Path> pathAllFile =new ArrayList();
-		ArrayList<Component> extComp =new ArrayList();
-		extComp.add(new Component("@"));
-		 
-	
-		  
-		int ctn=0;
-		try {
+			if(args.length!=0) {
+				System.out.print("usage: <program>");
+				return;
+			}
+			String outputFile=arguments.get(1)+"-output.json";
+			String path=arguments.get(0);
 			
-			Files.walk(new File(path).toPath())
-					.filter(Files::isRegularFile)
-					.forEach(it ->{pathAllFile.add(it);});
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+			getFile(path);
+			ArrayList<Path> pathAllFile =new ArrayList<Path>();
+			ArrayList<Component> extComp =new ArrayList<Component>();
+			 
 		
-		
-		for(int j=0;j<pathAllFile.size();j++) 
-		{
-			   String filePath=pathAllFile.get(j).toString();
-			   String extension=getExtension(filePath);
-			   System.out.println("Ext : "+extension);
-			  if(!extension.equals("")) 
-			  {
-				   int valid=1;
-				   for(int k=0;k<extComp.size();k++) 
-				   {
-					   if(extComp.get(k).getName().equals(extension))
+			  
+			try {
+				
+				Files.walk(new File(path).toPath())
+						.filter(Files::isRegularFile)
+						.forEach(it ->{pathAllFile.add(it);});
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			
+			for(int j=0;j<pathAllFile.size();j++) 
+			{
+				   String filePath=pathAllFile.get(j).toString();
+				   String extension=getExtension(filePath);
+				  if(!extension.equals("")) 
+				  {
+					   int valid=1;
+					   for(int k=0;k<extComp.size();k++) 
 					   {
-						   valid=0;
-						   break;
+						   if(extComp.get(k).getName().equals(extension))
+						   {
+							   valid=0;
+							   break;
+						   }
 					   }
-				   }
-				   if(valid==1) {
-					   extComp.add(new Component(extension));
-				   }
-			  }
-		}
-		 
-		for(int w=0;w<pathAllFile.size();w++) 
-		  {
-			  String extension=getExtension(pathAllFile.get(w).toString());
-			  for(int q=0;q<extComp.size();q++) 
-			   {
-				   if(extension.equals(extComp.get(q).getName()))
+					   if(valid==1) {
+						   extComp.add(new Component(extension));
+					   }
+				  }
+			}
+			 
+			for(int w=0;w<pathAllFile.size();w++) 
+			  {
+				  String extension=getExtension(pathAllFile.get(w).toString());
+				  for(int q=0;q<extComp.size();q++) 
 				   {
-					  extComp.get(q).updateFile(pathAllFile.get(w).toString());
-				   }   
+					   if(extension.equals(extComp.get(q).getName()))
+					   {
+						  extComp.get(q).updateFile(pathAllFile.get(w).toString());
+					   }   
+				   }
 			   }
-		   }
-		    
-		   JSONArray objF = new JSONArray();
-		   for(int h=0;h<extComp.size();h++) {
-			   JSONObject obj = new JSONObject();
-			   obj.put("name", extComp.get(h).getName());
-			   obj.put("files", extComp.get(h).getFiles());
-			   obj.put("category", "Extension");
-			   obj.put("value", extComp.get(h).getSize());
-			   objF.put(obj); 
-		   }
-		   
-		   try(FileWriter file = new FileWriter(outputFile))
-		   {
-			file.write(objF.toString());
-			file.flush();
-		   }catch(IOException e) {
-			   e.printStackTrace();
-		   }
-		   System.out.println(objF);
-		} 
+			    
+			   JSONArray objF = new JSONArray();
+			   for(int h=0;h<extComp.size();h++) {
+				   JSONObject obj = new JSONObject();
+				   obj.put("name", extComp.get(h).getName());
+				   obj.put("files", extComp.get(h).getFiles());
+				   obj.put("category", "Extension");
+				   obj.put("value", extComp.get(h).getSize());
+				   objF.put(obj); 
+			   }
+			   
+			   try(FileWriter file = new FileWriter(outputFile))
+			   {
+				file.write(objF.toString());
+				file.flush();
+				System.out.println("Extracted "+pathAllFile.size()+" files from project "+arguments.get(1));
+			   }catch(IOException e) {
+				   e.printStackTrace();
+			   }
+			   
+			
+		}
 		   
 	}
 	
@@ -134,5 +136,32 @@ public class mainClient {
 		    else
 		        return fileName.substring(dotInd+1).toLowerCase();
 		}
+  private static ArrayList<String> readPropertiesFile()
+  {
+	try {
+		File file = new File("config.properties");
+		FileInputStream inputStream = new FileInputStream(file);
+		Properties properties = new Properties();
+		properties.load(inputStream);
+		inputStream.close();
+		
+		Enumeration keys = properties.keys();
+		System.out.println("Reading property file: "+file.getName());
+		ArrayList<String> arguments = new ArrayList<String>();
+		while(keys.hasMoreElements())
+		{
+			String key = (String)keys.nextElement(); //projectName and projectPath
+			String value = properties.getProperty(key);
+			arguments.add(value);
+		}
+	return arguments;
+	}
+	catch(Exception e)
+	{
+		System.out.print(e.getMessage());
+		return null;
+	}
+	
+  }
 	
 }
